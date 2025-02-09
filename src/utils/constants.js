3ce697b1373615cbd3127afe15b40b12889f8f02
sajan moon <sajanmoon5@gmail.com>
2025-02-09 11:15:05 +0530

    baseurl

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
 // export const BASE_URL = "http://localhost:3000";
 export const BASE_URL = "https://devtinder-c5wb.onrender.com";
+
+// export const BASE_URL =
+//   location.hostname === "localhost" ? "http://localhost:7777" : "/api";
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,3 +4,14 @@ import { BASE_URL } from "./constants";
 export const createSocketConnection = () => {
   return io(BASE_URL);
 };
+
+// import io from "socket.io-client";
+// import { BASE_URL } from "./constants";
+
+// export const createSocketConnection = () => {
+//   if (location.hostname === "localhost") {
+//     return io(BASE_URL);
+//   } else {
+//     return io("/", { path: "/api/socket.io" });
+//   }
+// };
